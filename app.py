f4eb7060ebe4cdcaa46733b7b42d55f4c3b24d96
Paddhati Singh <your-real-email@gmail.com>
2026-04-08 17:10:24 +0530

    added gunicorn

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
 import json
 import pickle
-
-from flask import Flask,request,app,jsonify,url_for,render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
 
-app=Flask(__name__)
-## Load the model
-regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
+app = Flask(__name__)
+
+# Load model
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scalar = pickle.load(open('scaling.pkl', 'rb'))
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
-    print(output[0])
+    data = request.json['data']
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
     return jsonify(output[0])
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    output = regmodel.predict(final_input)[0]
+    return render_template("index.html", prediction_text=f"The House price prediction is {output}")
 
-
-
-if __name__=="__main__":
-    app.run(debug=True)
-   
-     
+# 👇 IMPORTANT for Render
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
